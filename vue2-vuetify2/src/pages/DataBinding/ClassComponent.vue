954a31d8f1b99286e58db8cf6f5e92f752fe87ba
UEDA Kenshiro <ueda@communitio.net>
2024-05-22 15:07:34 +0900

    PropSync デコレータの使い方を修正

--- vue2-vuetify2/src/pages/DataBinding/ClassComponent.vue
+++ vue2-vuetify2/src/pages/DataBinding/ClassComponent.vue
@@ -4,6 +4,7 @@
     <v-btn v-on:click="$emit('update:val', 'xyz')">update value</v-btn>
     <v-btn v-on:click="$emit('update:arr', [ 'x', 'y', 'z' ])">update array</v-btn>
     <v-btn v-on:click="$emit('update:obj', { a: 'x', b: 'y', c: 'z' })">update obj</v-btn>
+    <v-text-field v-model="$_val"></v-text-field>
   </v-form>
 </template>
 
@@ -12,8 +13,8 @@ import { Vue, Component, PropSync } from "vue-property-decorator";
 
 @Component
 export default class ClassComponent extends Vue {
-  @PropSync({ required: true }) val!: string;
-  @PropSync({ required: true }) arr!: string[];
-  @PropSync({ required: true }) obj!: Record<string, string>;
+  @PropSync("val", { required: true }) $_val!: string;
+  @PropSync("arr", { required: true }) $_arr!: string[];
+  @PropSync("obj", { required: true }) $_obj!: Record<string, string>;
 }
 </script>
--- vue2-vuetify2/src/pages/DataBinding/OptionsApi.vue
+++ vue2-vuetify2/src/pages/DataBinding/OptionsApi.vue
@@ -4,6 +4,7 @@
     <v-btn v-on:click="$emit('update:val', 'XYZ')">update value</v-btn>
     <v-btn v-on:click="$emit('update:arr', [ 'X', 'Y', 'Z' ])">update array</v-btn>
     <v-btn v-on:click="$emit('update:obj', { a: 'X', b: 'Y', c: 'Z' })">update obj</v-btn>
+    <v-text-field v-model="$_val"></v-text-field>
   </v-form>
 </template>
 
@@ -23,6 +24,13 @@ export default defineComponent({
     obj: {
       type: Object as PropType<Record<string, string>>,
       required: true,
+    },
+  },
+
+  computed: {
+    $_val: {
+      get() { return this.val },
+      set(value: string) { this.$emit('update:val', value) },
     },
   }
 });
